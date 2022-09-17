d67f7bd06f0a41f9a5d60ee9375bcccd3c7b74f8
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2022-09-17 01:04:52 -0500

    Creación de interfaz AlertActivity

--- app/src/main/java/com/alarmavecinal/app/MainActivity.java
+++ app/src/main/java/com/alarmavecinal/app/MainActivity.java
@@ -2,13 +2,20 @@ package com.alarmavecinal.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, AlertActivity.class);
+        startActivity(intent);
     }
 }
