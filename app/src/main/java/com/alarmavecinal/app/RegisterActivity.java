b3f1e83a5604e7e77c0ed6ae3b34ccff308644bf
Programador-Clever <ryuzetsuxx@gmail.com>
2022-09-17 01:11:57 -0500

    Registro de usuario y grupo

--- app/src/main/java/com/alarmavecinal/app/RegisterActivity.java
+++ app/src/main/java/com/alarmavecinal/app/RegisterActivity.java
@@ -2,16 +2,13 @@ package com.alarmavecinal.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button bt ;
+public class RegisterActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
