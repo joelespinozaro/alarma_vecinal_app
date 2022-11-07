c649e2a24f8ace3f80a14038cf63ebfa5e2ca651
Joel Espinoza <joelespinozaro@gmail.com>
2022-11-06 23:49:23 -0500

    registro de usuario - basic

--- app/src/main/java/com/alarmavecinal/app/MainActivityUser.java
+++ app/src/main/java/com/alarmavecinal/app/MainActivityUser.java
@@ -15,6 +15,8 @@ import com.alarmavecinal.app.fragmentos.CrearGrupo;
 import com.alarmavecinal.app.fragmentos.Inicio;
 import com.alarmavecinal.app.fragmentos.Perfil;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivityUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
@@ -59,7 +61,6 @@ public class MainActivityUser extends AppCompatActivity implements NavigationVie
             case R.id.Salir:
                 Toast.makeText(this, "Cerraste Sesion", Toast.LENGTH_SHORT).show();
                 break;
-
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
--- app/src/main/java/com/alarmavecinal/app/RegisterActivity.java
+++ app/src/main/java/com/alarmavecinal/app/RegisterActivity.java
@@ -1,9 +1,11 @@
 package com.alarmavecinal.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,12 +14,23 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText celular, nombre, apellido;
     Button registro;
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("sampleData/usuario");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +50,29 @@ public class RegisterActivity extends AppCompatActivity {
         if(account!=null){
             String name = account.getDisplayName();
             String lastName = account.getGivenName();
-
             nombre.setText(name);
             apellido.setText(lastName);
         }
+    }
+
+    public void onSave(View view) {
+        EditText nombreView = (EditText) findViewById (R.id.nombre);
+        EditText apellidoView = (EditText) findViewById (R.id.apellido);
+        String name = nombreView.getText ().toString();
+        String lastName = apellidoView.getText ().toString();
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+        dataToSave.put("name", name); dataToSave.put("lastname", lastName);
+        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("TAG", "usuario registrado correctamente.");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("TAG", "usuario registrado correctamente.");
+            }
+        });
     }
     public void Registrar(View view){
         Intent intent = new Intent(this,MainActivityUser.class);
