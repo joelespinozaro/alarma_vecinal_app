c922839ebca54f00b8f7163b10f85ba7bfc45115
Joel Espinoza <joelespinozaro@gmail.com>
2022-11-13 22:13:28 -0500

    Mejora en identificacion de sesion y Actualizacion en Perfil fragment

--- app/src/main/java/com/alarmavecinal/app/SesionActivity.java
+++ app/src/main/java/com/alarmavecinal/app/SesionActivity.java
@@ -4,24 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.alarmavecinal.app.model.User;
+import com.alarmavecinal.app.service.UserService;
+
 public class SesionActivity extends AppCompatActivity {
+    private User user;
     Button sesion, registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sesion);
         sesion = (Button) findViewById(R.id.InicioSesion);
+
     }
+
+    public void goToMainActivity() {
+        finish();
+        Intent intent = new Intent(this, MainActivityUser.class);
+        startActivity(intent);
+    }
+
     public void Sesion(View view){
         Intent intent = new Intent(this, ValidarNumActivity.class);
-        startActivity(intent);
-        finish();
-    }
-    public void Registrar(View view){
-        Intent intent = new Intent(this,RegisterActivity.class);
         startActivity(intent);
         finish();
     }
