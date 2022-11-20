f1b1e2e89faa3a462c09e752c49606305b59f791
Dylan <dylan.java.ra@gmail.com>
2022-11-19 19:04:29 -0500

    creando grupo

--- app/src/main/java/com/alarmavecinal/app/fragmentos/CrearGrupo.java
+++ app/src/main/java/com/alarmavecinal/app/fragmentos/CrearGrupo.java
@@ -22,27 +22,27 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class CrearGrupo extends Fragment {
     private EditText NombreGroupLayout,DescripcionGroupLayout,ContactGroupLayout;
-    private String CodigoGroup;
+    private Integer CodigoGroup;
     private FirebaseFirestore mFireStore;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.fragment_crear_grupo, container, false);
         mFireStore = FirebaseFirestore.getInstance();
         Button btnNewGroup = (Button) view.findViewById(R.id.guardarGrupo);
         NombreGroupLayout = (EditText) view.findViewById(R.id.nameGroup);
         DescripcionGroupLayout = (EditText) view.findViewById(R.id.descripcion);
-        ContactGroupLayout = (EditText) view.findViewById(R.id.contact);
+        //ContactGroupLayout = (EditText) view.findViewById(R.id.contact);
 
         btnNewGroup.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                generarCodigoGrupo();
+                setRandomCodeGroup();
                 newGroup();
             }
         });
@@ -52,26 +52,36 @@ public class CrearGrupo extends Fragment {
     public void newGroup(){
         String nombreGrupo =  this.NombreGroupLayout.getText().toString();
         String descripcionGrupo =  this.DescripcionGroupLayout.getText().toString();
-        String contactoGrupo =  this.ContactGroupLayout.getText().toString();
+        //String contactoGrupo =  this.ContactGroupLayout.getText().toString();
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("codgru",this.CodigoGroup);
-        map.put("nombre",nombreGrupo);
-        map.put("descripcion",descripcionGrupo);
+        if(!nombreGrupo.matches("") && !descripcionGrupo.matches("")){
+            Map<String, Object> map = new HashMap<>();
+            map.put("codgru",this.CodigoGroup);
+            map.put("nombre",nombreGrupo);
+            map.put("descripcion",descripcionGrupo);
 
-        this.mFireStore.collection("grupos").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Toast.makeText(getActivity(),"Grupo Creado Exitosamente",Toast.LENGTH_LONG ).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(),"Error al crear grupo",Toast.LENGTH_LONG ).show();
-            }
-        });
+            this.mFireStore.collection("grupos").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Toast.makeText(getActivity(),"Grupo Creado Exitosamente",Toast.LENGTH_LONG ).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getActivity(),"Error al crear grupo",Toast.LENGTH_LONG ).show();
+                }
+            });
+        }else{
+            Toast.makeText(getActivity(),"Complete los campos",Toast.LENGTH_LONG ).show();
+        }
+
+
 
     }
+    public void setRandomCodeGroup(){
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+        this.CodigoGroup = random.nextInt(10000);
+    }
 
-    public void generarCodigoGrupo(){        this.CodigoGroup = "1234L";}
 }
